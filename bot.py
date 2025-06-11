8cc908623fcdc6a9d1b3533322ed4b4b989b2060
meng-luo <admin@mengluo.work>
2025-06-11 20:54:40 +0800

    🦄 refactor(init): 整合bot初始化

--- bot.py
+++ bot.py
@@ -10,10 +10,9 @@ driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 
 # 在这里加载插件
-from mengluo_vrc_bot.services.db import init_db  # 导入初始化数据库的函数
+
 
 nonebot.load_plugins("mengluo_vrc_bot/plugins")  # 本地插件
 
 if __name__ == "__main__":
-    init_db()
     nonebot.run()
---
+++ mengluo_vrc_bot/plugins/init/__init__.py
@@ -0,0 +1,4 @@
+from mengluo_vrc_bot.services.db import init_db  # 导入初始化数据库的函数
+import mengluo_vrc_bot.config.path
+
+init_db()
